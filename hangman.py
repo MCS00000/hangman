8a2284ba4af68ad841b2a8bec13a5cc2b8293c80
Valdemārs Stokmanis-Blaus <v.stokmanis.blaus@gmail.com>
2021-11-08 21:33:06 +0200

    Izņēmumu apstrādes pievienošana
    Co-authored-by: Aleksandrs  Mikulāns <Aleksandrs.Mikulans@fita.lv>
    Co-authored-by: Maija Delvere <Maija.Delvere@fita.lv>

--- hangman.py
+++ hangman.py
@@ -1,5 +1,4 @@
 from gameplay.game2 import Game
-from scripts.difficulty import check_difficulty
 from random import shuffle
 from scripts.timer import countdown
 
@@ -8,25 +7,46 @@ print('\nSveiciens karātavās!')
 while True:
     difficulty = input('\nIeraksti spēles grūtības pakāpi (viegli / vidēji / grūti): ').upper()
     if difficulty == 'VIEGLI':
-        with open('data/easy_words.txt', 'r', encoding = 'utf-8') as file:
-            words = file.read()
+        try:
+            with open('data/easy_words.txt', 'r', encoding = 'utf-8') as file:
+                words = file.read()
+        except FileNotFoundError:
+            print('\nKļūda! Nav atrasts fails "easy_words.txt"! Programma beidzas.')
+            raise SystemExit
+        break
     elif difficulty == 'VIDĒJI':
-        with open('data/medium_words.txt', 'r', encoding = 'utf-8') as file:
-            words = file.read()
+        try:
+            with open('data/medium_words.txt', 'r', encoding = 'utf-8') as file:
+                words = file.read()
+        except FileNotFoundError:
+            print('\nKļūda! Nav atrasts fails "medium_words.txt"! Programma beidzas.')
+            raise SystemExit
+        break
     elif difficulty == 'GRŪTI':
-        with open('data/hard_words.txt', 'r', encoding = 'utf-8') as file:
-            words = file.read()
+        try:
+            with open('data/hard_words.txt', 'r', encoding = 'utf-8') as file:
+                words = file.read()
+        except FileNotFoundError:
+            print('\nKļūda! Nav atrasts fails "hard_words.txt"! Programma beidzas.')
+            raise SystemExit
+        break
     else:
         print('Grūtības pakāpe tika nepareizi ievadīta, mēģini vēlreiz!')
         continue
-    print(f'\nJAUNA SPĒLES PARTIJA - būs {difficulty}!\n')
-    words = words.split('\n')
-    shuffle(words)
-    word = words.pop()
-    #check difficulty of word
-    #difficulty_score = check_difficulty(word)
-    #print result
-    #print(f'Vārda grūtības indekss: {difficulty_score}')
+
+if not len(words):
+    raise Exception('Kļūda! Ielasītais fails ir tukšs! Programma beidzas.')
+
+words = words.split('\n')
+shuffle(words)
+
+while True:
+    try:
+        word = words.pop()
+    except IndexError:
+        print('Vārdu vairāk nav! Spēle beidzas!')
+        break
+    print(f'\nJauna spēles partija ar grūtības pakāpi: {difficulty}!\n')
     game = Game(word)
     game.play()
     t = 5
